10c874ca1b05db398035784fb2aa4abcabc5232b
Henry Aguilar <henrywaguilar505@gmail.com>
2023-11-08 18:56:34 -0600

    update
    update 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
 <body class="bg-gray-100">
 
-    <header class="p-2 md:p-5 border-b bg-white shadow">
+    <header class="p-3 md:p-5 border-b bg-white shadow">
         <div class="container w-full mx-auto flex justify-between items-center">
             <a href="{{route('home')}}" class="md:text-3xl font-black">
                 DevStagram
@@ -22,7 +22,7 @@
 
             @auth()
             <nav class="flex gap-4 items-center justify-center">
-                <a class="hidden md:flex items-center gap-2 bg-white border p-2 
+                <a class="flex items-center gap-2 bg-white border p-2 
                 text-gray-600 rounded text-sm uppercase font-bold cursor-pointer" 
                 href="{{route('posts.create')}}">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -30,17 +30,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
                   </svg>
                   
-                    Crear
-                </a>
-
-                <a class="flex md:hidden ml-2 items-center gap-2 bg-white border p-2 
-                text-gray-600 rounded text-sm uppercase font-bold cursor-pointer" 
-                href="{{route('posts.create')}}">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
-                  </svg>
-
                 </a>
 
                 <a class="font-bold text-gray-600 text-sm flex gap-1" href="{{route('posts.index',auth()->user()->username)}}">
